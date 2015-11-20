1028169a8c52dc4969ee0666e60c1a88ea62c64c
K Kollmann <git@k.kollmann.moe>
2015-11-20 02:07:43 +0100

    add new view controllers

---
+++ lovedrecords/SearchArtistViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SearchArtistViewController.swift
+//  lovedrecords
+//
+//  Copyright (c) 2015 K Kollmann.
+//  All rights reserved.
+//
+
+import UIKit
+
+class SearchArtistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ lovedrecords/TopAlbumsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TopAlbumsViewController.swift
+//  lovedrecords
+//
+//  Copyright (c) 2015 K Kollmann.
+//  All rights reserved.
+//
+
+import UIKit
+
+class TopAlbumsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
