908946c71cfd8718f6fa02520162e5968768b2f8
K Kollmann <git@k.kollmann.moe>
2015-11-21 20:59:19 +0100

    move buildurl func to correct viewcontroller, use actual artist

--- lovedrecords/TopAlbumViewController.swift
+++ lovedrecords/TopAlbumViewController.swift
@@ -25,6 +25,8 @@ class TopAlbumViewController: UIViewController {
         // rounded corners for image
         albumCover.clipsToBounds = true
         albumCover.layer.cornerRadius = 15
+        
+        buildUrl()
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,14 +34,38 @@ class TopAlbumViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func lastfmLink(sender: AnyObject) {
+        if let url = NSURL(string: "https://last.fm") {
+            UIApplication.sharedApplication().openURL(url)
+        }
     }
-    */
-
+    
+    // build URL for last.fm request
+    func buildUrl() {
+        // last.fm get top album from given artist
+        // http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=Artist+Name&limit=1&autocorrect=1&api_key=KEY&format=json
+        
+        // placeholder artist
+        // replace all spaces with '+' signs
+        let artistInput = searchTerm
+        print("Requested artist: \n\(artistInput)\n")    // debug
+        
+        let artist = artistInput.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.LiteralSearch, range: nil)
+        
+        
+        // load last.fm key variable from Info.plist
+        if let lastfmKey = NSBundle.mainBundle().infoDictionary?["Last.fm API Key"] as? String {
+            print("Last.fm API Key: \n\(lastfmKey)\n")    // debug
+            let lastfmUrl: String
+            
+            // build url
+            lastfmUrl = "https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&" +
+                "artist=\(artist)" +
+                "&limit=1" +
+                "&autocorrect=1" +
+                "&api_key=\(lastfmKey)" +
+            "&format=json"
+            print("Full last.fm API request URL: \n\(lastfmUrl)\n")    // debug
+        }
+    }
 }
