6b1a731684be3ddbfda9e9da509ce4e72f8dad66
K Kollmann <git@k.kollmann.moe>
2015-11-21 23:45:50 +0100

    make segue dependent on field value on button press

--- lovedrecords/SearchArtistViewController.swift
+++ lovedrecords/SearchArtistViewController.swift
@@ -15,7 +15,9 @@ class SearchArtistViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var artistSearchField: UITextField!
     @IBOutlet weak var startSearchButton: UIButton!
     @IBOutlet weak var poweredBy: UILabel!
-
+    
+    let artistValue: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -29,29 +31,34 @@ class SearchArtistViewController: UIViewController, UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    // segue connecting
-    // SearchArtist with TopAlbum
+    // segue connecting SearchArtist with TopAlbum
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if (segue.identifier == "segueToAlbum") {
             let seg = segue.destinationViewController as! TopAlbumViewController
-            seg.searchTerm = self.artistSearchField.text!
+            seg.submittedValue = self.artistSearchField.text!
         }
     }
     
-    func textFieldShouldReturn(artistSearchField: UITextField) -> Bool {
+    // hide keyboard
+    func hideKeyboard(artistSearchField: UITextField) -> Bool {
         artistSearchField.resignFirstResponder()
         return true
     }
     
-    
-    /*
-    // MARK: - Navigation
+    // submit button press
+    @IBAction func submitArtist(sender: UIButton) {
+        let artistValue = artistSearchField.text
+        
+        print("Search field input:") // debug
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        // only activate the segue on non-empty input
+        if (artistValue == "") {
+            print("~EMPTY~\n") // debug
+        } else {
+            print("\(artistValue!)\n") // debug
+
+            hideKeyboard(artistSearchField)
+            performSegueWithIdentifier("segueToAlbum", sender: self)
+        }
     }
-    */
-
 }
