d8ea29189c8452f2541239c5b0294a31afee480f
K Kollmann <git@k.kollmann.moe>
2015-11-27 00:24:09 +0100

    refactor: segue only on non-empty text input (incl. pop-up), fix keyboard hiding, check last.fm url

--- lovedrecords/SearchArtistViewController.swift
+++ lovedrecords/SearchArtistViewController.swift
@@ -17,12 +17,14 @@ class SearchArtistViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var poweredBy: UIButton!
     
     let artistValue: String = ""
+    let lastfmUrl = "http://last.fm/"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // round corners for powered by label
         poweredBy.layer.cornerRadius = 4
+        self.artistSearchField.delegate = self;
     }
     
     override func didReceiveMemoryWarning() {
@@ -30,40 +32,63 @@ class SearchArtistViewController: UIViewController, UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    // segue connecting SearchArtist with TopAlbum
+    // activate segue only if artist search field is not empty
+    override func shouldPerformSegueWithIdentifier(identifier: String, sender: AnyObject?) -> Bool {
+        
+        if identifier == "showAlbumSegue" {
+            
+            // check for empty text field (can never be nil, but "")
+            guard let artist = artistSearchField.text where artist != "" else {
+                print("No artist provided.") // debug
+                
+                // let the user know they need to provide an artist name
+                let alert = UIAlertView()
+                alert.title = "Missing artist"
+                alert.message = "You forgot to enter an artist!"
+                alert.addButtonWithTitle("OK")
+                alert.show()
+                
+                return false
+            }
+            print("Let's go!") // debug
+            
+            // hide keyboard
+            textFieldShouldReturn(artistSearchField)
+        }
+        return true
+    }
+    
+    // open last.fm website on 'powered by' button click
+    @IBAction func goToLastfm(sender: AnyObject) {
+    
+        // check for validity + 'openability' of url
+        // (it's complicated...)
+        // (thus incomplete)
+        if let url = NSURL(string: lastfmUrl) {
+            print("Valid NSURL.") // debug
+
+            if UIApplication.sharedApplication().canOpenURL(url) {
+                print("Opening \(url)") // debug
+                UIApplication.sharedApplication().openURL(url)
+            } else {
+                print("Cannot open URL with default app.") // debug
+            }
+        } else {
+            print("Not a valid URL.") // debug
+        }
+    }
+    
+    // segue: hand over data to second view controller
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if (segue.identifier == "segueToAlbum") {
+        if (segue.identifier == "showAlbumSegue") {
             let seg = segue.destinationViewController as! TopAlbumViewController
             seg.submittedValue = self.artistSearchField.text!
         }
     }
     
-    @IBAction func goToLastfm(sender: AnyObject) {
-        if let url = NSURL(string: "https://last.fm") {
-            UIApplication.sharedApplication().openURL(url)
-        }
-    }
-    
     // hide keyboard
-    func hideKeyboard(artistSearchField: UITextField) -> Bool {
-        artistSearchField.resignFirstResponder()
-        return true
-    }
-    
-    // submit button press
-    @IBAction func submitArtist(sender: UIButton) {
-        let artistValue = artistSearchField.text
-        
-        print("Search field input:") // debug
-
-        // only activate the segue on non-empty input
-        if (artistValue == "") {
-            print("~EMPTY~\n") // debug
-        } else {
-            print("\(artistValue!)\n") // debug
-
-            hideKeyboard(artistSearchField)
-            performSegueWithIdentifier("segueToAlbum", sender: self)
-        }
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
     }
 }
